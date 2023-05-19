98da394e89c690e5c10705d41d6dd26628582168
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-05-19 20:54:42 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/style.css">
+    <title>Medico: One step solution for your medical needs</title>
+</head>
+<body>
+   <h1>medico</h1>
+</body>
+</html>
