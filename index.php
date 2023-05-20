8c34041f1574e6a14396c59e5e9934c6db41d4e6
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-05-20 09:58:47 +0530

    Completed homepage

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php session_start();?>
+<?php
+    session_start();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,9 +9,121 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/nav.css">
     <title>Medico: One step solution for your medical needs</title>
 </head>
 <body>
-   <h1>medico</h1>
+    <?php require_once "nav.php"; ?>
+    <section>
+        <div class="head flexbox">
+            <h1>Get Your Prescriptions Sorted</h1>
+            <form method="get" action="meds.php" class="get-med-form flexbox flexrow">
+            <input type="text" class="get-med" name="med" placeholder="Enter medication name" required>
+            <input type="submit" value="Search Nearby">
+            </form>
+        </div>
+    </section>
+    
+    <section>
+        <div class="site-save">
+            <h2>Save at pharmacies near you</h2>
+            <div class="pharm-row flexbox flexrow">
+                <div class="site-pharm">
+                    <h3>Saravan Pharmacy</h3>
+                    <hr>
+                    <ul>
+                        <li>Albuterol</li>
+                        <li>Aspirin</li>
+                        <li>Ibuprofen</li>
+                        <li>Amlodipine</li>
+                    </ul>
+                    <p class="pharm-p"><span style="color:green;">39%</span><span style="color:#666"> off with Medico</span></p>
+                    <a href="#">Save here</a>
+                </div>
+                <div class="site-pharm">
+                    <h3>Apollo Medicals</h3>
+                    <hr>
+                    <ul>
+                        <li>Naproxen Sodium</li>
+                        <li>Metoprolol</li>
+                        <li>Albuterol</li>
+                        <li>Paracetamol</li>
+                    </ul>
+                    <p class="pharm-p"><span style="color:green;">25%</span><span style="color:#666"> off with Medico</span></p>
+                    <a href="#">Save here</a>
+                </div>
+                <div class="site-pharm">
+                    <h3>Kayes Pharmacy</h3>  
+                    <hr>
+                    <ul>
+                        <li>Aspirin</li>
+                        <li>Albuterol</li>
+                        <li>Ibuprofen</li>
+                        <li>Metoprolol</li>
+                    </ul>
+                    <p class="pharm-p"><span style="color:green;">54%</span><span style="color:#666"> off with Medico</span></p>
+                    <a href="#">Save here</a>
+                </div>
+                <div class="site-pharm">
+                    <h3>Arun Pharma</h3>
+                    <hr>
+                    <ul>
+                        <li>Simvastatin</li>
+                        <li>Ibuprofen</li>
+                        <li>Naproxen Sodium</li>
+                        <li>Metformin</li>
+                    </ul>
+                    <p class="pharm-p"><span style="color:green;">15%</span><span style="color:#666"> off with Medico</span></p>
+                    <a href="#">Save here</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="common">
+            <h2>Common Health Conditions</h2>
+            <div class="com-row flexbox flexrow">
+                <div class="coms">
+                    <img src="https://www.grxstatic.com/d4fuqqd5l3dbz/static/img/photo-condition-allergies.jpg" alt="" class="com-img">
+                    <p>Allergies</p>
+                    <hr>
+                    <p class="common-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia eius aliquid nulla voluptatem, exercitationem porro quasi nisi quae a possimus.</p>
+                    <a href="#">Get Help</a>
+                </div>
+                <div class="coms">
+                    <img src="https://www.grxstatic.com/d4fuqqd5l3dbz/static/img/photo-condition-acne.jpg" alt="" class="com-img">
+                    <p>Acne</p>
+                    <hr>
+                    <p class="common-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt cumque dolore nam molestias, ducimus harum sed explicabo ipsa commodi soluta?</p>
+                    <a href="#">Get Help</a>
+                </div>
+                <div class="coms">
+                    <img src="https://www.grxstatic.com/d4fuqqd5l3dbz/static/img/photo-condition-flu.jpg" alt="" class="com-img">
+                    <p>Flu</p>
+                    <hr>
+                    <p class="common-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum aspernatur odio accusantium sequi veniam illum explicabo ipsum voluptatum incidunt saepe.</p>
+                    <a href="#">Get Help</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="head flexbox">
+            <h1>Get Medical Help</h1>
+            <form method="get" action="emergency.php" class="get-med-form flexbox flexrow">
+            <input type="text" class="get-med" name="med" placeholder="Enter your pincode (Eg. 632014)" required>
+            <input type="submit" value="Search Hospitals">
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer">
+            <p>Copyright© 2023 Medico,</p>
+            <p>All rights reserved</p>
+        </div>
+    </footer>
 </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,26 @@
+<header class="site-header flexbox flexrow" id="crest" role="banner">
+        <div class="site-branding flexbox">
+            <a href="index.php" class="custom-logo-link" rel="home" aria-current="page">
+                <img src="static/imgs/logo.png" alt="Medico Medical Help" style="max-width:192px;max-height:100px;">
+            </a>
+            <h1 class="site-title">
+                <a href="index.php" rel="home">Medico</a>
+            </h1>
+            <label for="nav-toggle" class="toggle-label flexbox">
+                <span class="toggle-icon"></span>
+                <span class="toggle-icon"></span>
+                <span class="toggle-icon"></span>
+            </label>
+        </div>
+        <input type="checkbox" name="nav-toggle" id="nav-toggle" class="nav-toggle">
+        <nav id="site-navigation" class="flexbox main-navigation" role="navigation">
+            <div class="flexbox primary-menu">
+                <ul class="flexbox flexrow">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="meds.php">MEDICINES</a></li>
+                    <li><a href="emergency.php">EMERGENCY</a></li>
+                    <li><a href="profile.php">PROFILE</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
